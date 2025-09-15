da2a0f163cb1b05ce1c7cf364c5603529ae4e5bb
akcods <ak20ash@gmail.com>
2025-09-15 22:53:33 +0530

    Created controller for getting weather forecast data

---
+++ AspNetCore.Identity/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[Controller]")]
+public class WeatherForecastController : ControllerBase
+{
+
+    [HttpGet]
+    public WeatherForecast[] GetWeatherForecasts()
+    {
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+        return forecast;
+    }
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
